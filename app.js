508aa849a2591699eeb5b68388f87408738c7162
Igor Wysocki <contact@igorwysocki.com>
2019-05-07 11:16:26 +0200

    add layout support for ejs in express

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const app = express();
+const expressLayouts=require("express-ejs-layouts");
+
 const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+app.use(expressLayouts);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
