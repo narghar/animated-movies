e6f92155f1265216e4f2c58fb3c4d2e0545bb359
Igor Wysocki <contact@igorwysocki.com>
2019-05-08 23:41:30 +0200

    add video header

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,30 +1,37 @@
 // Controller for all root / routes
 const movies = [
   {
+    id: 1,
     name:  "Spirited Away: W krainie bogów",
     image: "https://image.tmdb.org/t/p/w300/oRvMaJOmapypFUcQqpgHMZA6qL9.jpg"
   },
   {
+    id: 2,
     name:  "Spider-Man Uniwersum",
     image: "https://image.tmdb.org/t/p/w300/uWb1OWabm8YSoLp1JGeJl0fF7DT.jpg"
   },
   {
+    id: 3,
     name:  "Grobowiec świetlików",
     image: "https://image.tmdb.org/t/p/w300/a8S5d5w7tzNAgSHDEybOJPwSey4.jpg"
   },
   {
+    id: 4,
     name:  "Księżniczka Mononoke",
     image: "https://image.tmdb.org/t/p/w300/oGZmGO4Q9HhoRXGRNGKduYvtL9h.jpg"
   },
   {
+    id: 5,
     name:  "Król Lew",
     image: "https://image.tmdb.org/t/p/w300/nGArwXth0dOH8AUZuPCyBBJEWoF.jpg"
   },
   {
+    id: 6,
     name:  "Coco",
     image: "https://image.tmdb.org/t/p/w300/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg"
   },
   {
+    id: 7,
     name:  "Paperman",
     image: "https://image.tmdb.org/t/p/w300/3TpMBcAYH4cxCw5WoRacWodMTCG.jpg"
   },
