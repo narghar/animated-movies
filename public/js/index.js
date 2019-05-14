964d64e93a62926f831479797de540493f9b45f0
Igor Wysocki <narghar@gmail.com>
2019-05-14 23:43:12 +0200

    fix to play video only on search path

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,21 @@ let searchForm = document.querySelector('.search');
 let videoElem = document.querySelector('.bg-video__content');
 let searchButton = document.querySelector('.search__icon');
 
-// TODO: need changes, to play video as preloader for api search!
-searchForm.addEventListener('submit', e => {
-  e.preventDefault();
-  console.log('work');
-});
 
-searchButton.addEventListener("click", () => {
+// searchForm.addEventListener('submit', e => {
+//   e.preventDefault();
+//   console.log('work');
+// });
+if (window.location.pathname=='/search') {
   videoElem.play();
-});
+}
+// searchButton.addEventListener("click", () => {
+//   videoElem.play();
 
-searchForm.addEventListener('keypress', function(event) {
-  if (event.keyCode === 13 || event.which === 13) {
-      videoElem.play();
-    }
-});
+// });
+
+// searchForm.addEventListener('keypress', function(event) {
+//   if (event.keyCode === 13 || event.which === 13) {
+//       videoElem.play();
+//     }
+// });
